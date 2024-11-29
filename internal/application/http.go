c7d11f9658f91d6d50f95cf3862cbcf884e57af1
Igor Ivanov <igor.ivanov.it@mail.ru>
2024-11-29 17:06:00 +0700

    refactor: declare interfaces in files where they use

--- internal/application/http.go
+++ internal/application/http.go
@@ -1,19 +1,31 @@
 package application
 
 import (
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
+	"net"
 
 	"github.com/SpaceSlow/test-task-backend-junior-medods/generated/openapi"
-	"github.com/SpaceSlow/test-task-backend-junior-medods/internal/application/users"
+	usersapp "github.com/SpaceSlow/test-task-backend-junior-medods/internal/application/users"
+	"github.com/SpaceSlow/test-task-backend-junior-medods/internal/domain/users"
 )
 
+type UserService interface {
+	Tokens(userGUID uuid.UUID, ip net.IP) (*users.AccessToken, *users.RefreshToken, error)
+	RefreshTokens(access *users.AccessToken, refresh *users.RefreshToken, ip net.IP) (*users.AccessToken, *users.RefreshToken, error)
+}
+
+type NotifierService interface {
+	SendSuspiciousActivityMail(email string, newIP net.IP) error
+}
+
 type httpServer struct {
-	users.UserHandlers
+	usersapp.UserHandlers
 }
 
 func SetupHTTPServer(userService UserService) *echo.Echo {
 	server := &httpServer{}
-	server.UserHandlers = users.SetupHandlers(userService)
+	server.UserHandlers = usersapp.SetupHandlers(userService)
 
 	router := echo.New()
 	api := router.Group("/api")
